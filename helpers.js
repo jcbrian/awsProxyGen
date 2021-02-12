4ff4b21314ac894b13c745fd19fcc4cf1cdcd7ec
alive <42327548+jcbrian@users.noreply.github.com>
2021-02-12 04:40:10 -0500

    fixed logger

--- helpers.js
+++ helpers.js
@@ -27,5 +27,28 @@ function generateRandomId(prefix, addLetter, exsitIds) {
       }, time);
     });
   }
+function log(message, color = "lightpurple")
+{
+	const colors = {
+		"error" 		: "\\033[91m",
+		"success" 		: "\\033[92m",
+		"info" 			: "\\033[96m",
+		"debug" 		: "\\033[95m",
+		"yellow" 		: "\\033[93m",
+		"lightpurple" 	: "\\033[94m",
+		"lightgray" 	: "\\033[97m",
+		"reset"			: "\\033[00m"
+	}
+
+	let msg = "";
+
+	if(color) {
+		msg += colors[color] + message + colors["reset"]
+	} else {
+		msg = message
+	}
+
+	console.log(`[${moment().format('hh:mm:ss:SS')}] ${msg}`)
+}
 
   module.exports = {generateRandomId, sleep,log}
